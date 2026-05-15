1161ac9426c1e2cdcf879caab1a1cfe674cf63a2
Sujal Shahi <sshahi13@myseneca.ca>
2026-05-15 01:42:01 -0400

    completed lab2

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Sujal Shahi
+# Author ID: sshahi13
+# Date Created: 2026/05/15
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
---
+++ lab2g.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: Sujal Shahi
+# Author ID: sshahi13
+# Date Created: 2026/05/15
+
+import sys
+
+if len(sys.argv) == 2:
+    timer = int(sys.argv[1])
+else:
+    timer = 3
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
